8da501fd2a7b163254996f917ee1de684e4803bf
erozonachi <erozonachi.james@gmail.com>
2019-05-06 19:56:48 +0100

    Create object methods
    create kennan.speak() method that allows kennan to greet and say his name, create antonietta.multiplyNums(num1, num2) method that enables antonietta to multiply two numbers.

--- assignments/objects.js
+++ assignments/objects.js
@@ -74,10 +74,12 @@ console.log(`Antonietta's Gender: ${antonietta.gender}`);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+kennan.speak = () => `Hello, my name is ${kennan.name}!`;
+console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+antonietta.multiplyNums = (num1, num2) => num1 * num2;
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
